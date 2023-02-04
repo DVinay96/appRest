446c98183a1fbd98b4c3aeb19d93023c1f1d4636
DVinay96 <114019980+DVinay96@users.noreply.github.com>
2023-02-04 15:48:12 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header';
+import Jumbotron from './Components/Jumbotron/Jumbotron';
+
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <Header/>
+      <Jumbotron/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
+
